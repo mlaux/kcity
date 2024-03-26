6efd98042fe9206c7d9fecca0b7f6df57339e4ad
Matthew Laux <matthew.laux@gmail.com>
2024-03-26 16:27:29 -0500

    generate font tiles? idk

--- font/make-font.py
+++ font/make-font.py
@@ -1,12 +1,12 @@
 # magick -font FindersKeepers -pointsize 16 label:abcde test.png
-# trim 3 from top, 1 from right, 1 from bottom
 import subprocess
 
 def main():
-  widths = []
 
-  for ch in range(33, 128):
+  for ch in range(32, 128):
     label = chr(ch)
+    if ch == ord(' '):
+      label = '.'
     if ch == ord('\\'):
       label = '\\\\'
     subprocess.run([
@@ -16,20 +16,21 @@ def main():
       "-pointsize",
       "16",
       f"label:{label}",
-      "-crop", "+0+3", "+repage",
-      "-crop", "-1+0", "+repage",
-      "-crop", "+0-1", "+repage",
-      f"{ch}.png"
+      "-bordercolor",
+      "white",
+      "-border",
+      "1",
+      # "-crop", "+0+2", "+repage",
+      # "-crop", "-1+0", "+repage",
+      # "-crop", "+0-1", "+repage",
+      "-trim", "+repage",
+      "-gravity", "northwest", "-extent", "8x8",
+      f"ch{ch:03d}.png"
     ])
-    out = subprocess.run([
-      "identify",
-      "-ping",
-      "-format",
-      "%w",
-      f"{ch}.png"
-    ], capture_output=True)
-    widths.append(int(out.stdout))
-  print(widths)
+
+  subprocess.run([
+    "montage ch*.png +set label -tile 16x -geometry 8x8+0+0 geneva.png"
+  ], shell=True)
 
 if __name__ == '__main__':
   main()
